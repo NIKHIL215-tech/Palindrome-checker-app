ea8a02376b67a9a5d0b3ad1777ed421410d2ea73
Nikhil Singh <ns0546@srmist.edu.in>
2026-03-10 00:57:52 +0530

    UC9-Palindrome completed

--- App/src/PalindromeCheckerApp.java
+++ App/src/PalindromeCheckerApp.java
@@ -1,56 +1,32 @@
 import java.util.Scanner;
 
-class Node {
-    char data;
-    Node next;
-    Node(char data) { this.data = data; }
-}
-
 public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a string: ");
         String input = scanner.nextLine();
 
-        Node head = null, tail = null;
-        for (char ch : input.toCharArray()) {
-            Node newNode = new Node(Character.toLowerCase(ch));
-            if (head == null) { head = newNode; tail = newNode; }
-            else { tail.next = newNode; tail = newNode; }
+        boolean isPalindrome = checkRecursive(input, 0, input.length() - 1);
+
+        if (isPalindrome) {
+            System.out.println("Result: '" + input + "' is a palindrome.");
+        } else {
+            System.out.println("Result: '" + input + "' is not a palindrome.");
         }
 
-        Node slow = head, fast = head;
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-        }
-
-        Node secondHalf = reverseList(slow);
-        Node firstHalf = head;
-
-        boolean isPalindrome = true;
-        Node tempSecond = secondHalf;
-        while (tempSecond != null) {
-            if (firstHalf.data != tempSecond.data) {
-                isPalindrome = false;
-                break;
-            }
-            firstHalf = firstHalf.next;
-            tempSecond = tempSecond.next;
-        }
-
-        System.out.println("Result: '" + input + "' is " + (isPalindrome ? "" : "not ") + "a palindrome.");
         scanner.close();
     }
 
-    private static Node reverseList(Node head) {
-        Node prev = null, current = head, next = null;
-        while (current != null) {
-            next = current.next;
-            current.next = prev;
-            prev = current;
-            current = next;
+    public static boolean checkRecursive(String str, int start, int end) {
+        if (start >= end) {
+            return true;
         }
-        return prev;
+
+        if (Character.toLowerCase(str.charAt(start)) != Character.toLowerCase(str.charAt(end))) {
+            return false;
+        }
+
+        return checkRecursive(str, start + 1, end - 1);
     }
 }
